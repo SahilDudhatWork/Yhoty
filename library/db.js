5f848831783010158b4da43edb65855ddd421bd4
sahil dudhat <sahildudhat03@gmail.com>
2025-01-23 15:34:13 +0530

    mongo url

--- library/db.js
+++ library/db.js
@@ -5,9 +5,9 @@ const MongoDBconnect = async () => {
   console.log("MongoURL <--->", process.env.MONGO_URI);
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("Mongoose Successfully connected to MongoDB");
+    console.log("✅ Mongoose Successfully connected to MongoDB");
   } catch (error) {
-    console.error("Mongoose Connection Error:", error);
+    console.error("❌ Mongoose Connection Error:", error);
   }
 };
 
