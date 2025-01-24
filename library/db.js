c0e5a70db092d3aa29e0037b7ff14450a8cf31ec
sahil dudhat <sahildudhat03@gmail.com>
2025-01-24 12:35:13 +0530

    fix connectin err

--- library/db.js
+++ library/db.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 
 const MongoDBconnect = async () => {
   console.log("MongoURL <--->", process.env.MONGO_URI);
+  mongoose.set("strictQuery", false);
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ Mongoose Successfully connected to MongoDB");
