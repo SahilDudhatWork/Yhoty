e32fa407daf11941b9acefe2b6788256acf02c15
sahil dudhat <sahildudhat03@gmail.com>
2025-01-27 10:42:06 +0530

    swagger update 4

--- routes/swagger/index.js
+++ routes/swagger/index.js
@@ -32,9 +32,24 @@ router.get("/v1/swagger/user/api-docs/swagger-ui.css", (req, res) => {
   );
 });
 
+router.get("/v1/swagger/user/api-docs/swagger-ui.css.map", (req, res) => {
+  res.sendFile(
+    path.join(
+      __dirname,
+      "../../node_modules/swagger-ui-dist/swagger-ui.css.map"
+    )
+  );
+});
+
 router.get("/v1/swagger/user/api-docs/favicon-16x16.png", (req, res) => {
   res.sendFile(
     path.join(__dirname, "../../node_modules/swagger-ui-dist/favicon-16x16.png")
+  );
+});
+
+router.get("/v1/swagger/user/api-docs/favicon-32x32.png", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../../node_modules/swagger-ui-dist/favicon-32x32.png")
   );
 });
 
@@ -46,5 +61,26 @@ router.get("/v1/swagger/user/api-docs/swagger-ui-init.js", (req, res) => {
     )
   );
 });
+
+router.get("/v1/swagger/user/api-docs/swagger-ui-bundle.js", (req, res) => {
+  res.sendFile(
+    path.join(
+      __dirname,
+      "../../node_modules/swagger-ui-dist/swagger-ui-bundle.js"
+    )
+  );
+});
+
+router.get(
+  "/v1/swagger/user/api-docs/swagger-ui-standalone-preset.js",
+  (req, res) => {
+    res.sendFile(
+      path.join(
+        __dirname,
+        "../../node_modules/swagger-ui-dist/swagger-ui-standalone-preset.js"
+      )
+    );
+  }
+);
 
 module.exports = router;
