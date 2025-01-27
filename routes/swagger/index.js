cb1b3f7089d592722114f7b0d9d6f5c2e7308afb
sahil dudhat <sahildudhat03@gmail.com>
2025-01-27 10:48:14 +0530

    swagger update 5

--- routes/swagger/index.js
+++ routes/swagger/index.js
@@ -4,26 +4,19 @@ const swaggerUi = require("swagger-ui-express");
 const fs = require("fs");
 const path = require("path");
 
-// Load Swagger JSON Files
-const adminSwaggerPath = path.join(
-  __dirname,
-  "../../swaggerAi/admin_openapi.json"
-);
-const userSwaggerPath = path.join(
-  __dirname,
-  "../../swaggerAi/user_openapi.json"
-);
+const adminSwagger = require("../../swaggerAi/admin_openapi.json");
+const userSwagger = require("../../swaggerAi/user_openapi.json");
 
-// Read JSON Files Dynamically (Ensure they exist)
-const adminSwagger = fs.existsSync(adminSwaggerPath)
-  ? JSON.parse(fs.readFileSync(adminSwaggerPath, "utf-8"))
-  : {};
-const userSwagger = fs.existsSync(userSwaggerPath)
-  ? JSON.parse(fs.readFileSync(userSwaggerPath, "utf-8"))
-  : {};
-
-router.use("/admin/api-docs", swaggerUi.serve, swaggerUi.setup(adminSwagger));
-router.use("/user/api-docs", swaggerUi.serve, swaggerUi.setup(userSwagger));
+router.use(
+  "/admin/api-docs",
+  swaggerUi.serveFiles(adminSwagger),
+  swaggerUi.setup(adminSwagger)
+);
+router.use(
+  "/user/api-docs",
+  swaggerUi.serveFiles(userSwagger),
+  swaggerUi.setup(userSwagger)
+);
 
 // Serve Swagger UI Assets
 router.get("/v1/swagger/user/api-docs/swagger-ui.css", (req, res) => {
